f2f6f40e875aeb982cbb0a2747651e53509c5e06
Emmanuel Arias <eamanu@yaerobi.com>
2021-02-07 22:42:54 -0300

    add cli

---
+++ indep_free/cli.py
@@ -0,0 +1,12 @@
+from indep_free.manager import download_newspaper
+from pathlib import Path
+
+
+def main():
+    # Need customize work
+    folder = Path('/usr/share/indep_free/')
+    download_newspaper(folder, False)
+
+
+if __name__ == '__main__':
+    main()
